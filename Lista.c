f6f00ae42c1871a748f335175cc75070ddb3b88b
VitorZ-Pamplona <111309532+VitorZ-Pamplona@users.noreply.github.com>
2022-09-13 21:09:15 -0300

    Update Lista.c

--- Lista.c
+++ Lista.c
@@ -9,113 +9,115 @@ Descrição: Arquivo do exercicio de programação -> Lista_sequencial
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-}
-
 void cria_lista(tlista *L, int tam) {
-  L->dados = (int *)malloc(tam * sizeof(int));
-  L->n = 0;
-  L->nmax = tam;
+    L->dados = (float *)malloc(tam * sizeof(int));
+    L->n = 0;
+    L->nmax = tam;
 }
 
 int verifica_vazia(tlista *L) {
-  return (L->n == 0); }
+    if (L->n == 0){
+        return printf ("Lista vazia.\n");
+    } else {
+        return printf ("Lista Cheia ou invalida!\n");
+    }
+}
 
 int verifica_cheia(tlista *L) {
-  return (L->n == L->nmax); }
+    if (L->n == L->nmax){
+        return printf ("Lista Cheia.\n");
+    } else {
+        return printf ("Lista com espaço.\n");
+    }
+}
 
 int tamanho(tlista *L) {
-  return (L->n); }
+    return printf ("Tamanho da lista: %i.\n", (L->n)); 
+}
 
 int obter_elemento(tlista *L, int pos) {
-  int dado;
-  if ((pos < L->n) || (pos >= 0)) {
-    dado = L->dados[pos - 1];
-    return dado;
-  } else {
-    return -1;
-  }
-}
-
-int obter_posicao(tlista *L, int dado) {
-  for (int i = 0; i < L->n; i++) {
-    if (L->dados[i] == dado) {
-      return (i + 1);
+    int dado;
+    if ((pos < L->n) || (pos >= 0)) {
+        dado = L->dados[pos - 1];
+        return printf ("O elemento é : %f\n" ,dado);
+    } 
+    else {
+        return printf ("Erro ao obter o elemento!\n");
     }
-  }
-  return -1;
 }
 
-int insere(tlista *L, int pos, int dado) {
-  if (verifica_cheia(L) || (pos > L->nmax + 1) || (pos <= 0)) {
-    return -1;
-  }
-  for (int i = L->n; i >= pos; i--) {
-    L->dados[i] = L->dados[i - 1];
-  }
+int obter_posicao(tlista *L, float dado) {
+    for (int i = 0; i < L->n; i++) {
+        if (L->dados[i] == dado) {
+            return printf ("A posição é %d\n" , (i + 1));
+        }
+    }
+    return printf ("Erro ao obter a posição!\n");
+}
 
-  L->dados[pos - 1] = dado;
-  L->n++;
+int insere(tlista *L, int pos, float dado) {
+    if (verifica_cheia(L) || (pos > L->nmax + 1) || (pos <= 0)) {
+        return printf ("Erro ao Inserir o elemento!\n");
+    }
+    for (int i = L->n; i >= pos; i--) {
+        L->dados[i] = L->dados[i - 1];
+    }
+    L->dados[pos - 1] = dado;
+    L->n++;
+    return ("Elemento inserido com sucesso!\n");
 }
 
 int retira(tlista *L, int pos) {
-  int dado;
-  if ((pos > L->n) || (pos < 1)) {
-    return -1;
-  }
-
-  dado = L->dados[pos - 1];
-  for (int i = pos - 1; i < L->n - 1; i++) {
-    L->dados[i] = L->dados[i + 1];
-  }
-  L->n--;
-  return dado;
+    int dado;
+    if ((pos > L->n) || (pos < 1)) {
+        return printf ("Erro ao retirar o elemento!\n");
+    }
+    
+    dado = L->dados[pos - 1];
+    for (int i = pos - 1; i < L->n - 1; i++) {
+        L->dados[i] = L->dados[i + 1];
+    }
+    L->n--;
+    return printf ("%f retirado com sucesso!\n",dado);
 }
 
-int menu() {
-  int opcao;
-
-  printf("\n         ***   MENU   ***\n");
-  printf("||           Cria Lista           [1]||\n");
-  printf("|| Verifica se a lista esta vazia [2]||\n");
-  printf("|| Verifica se a lista esta cheia [3]||\n");
-  printf("||    Verifica tamanho da Lista   [4]||\n");
-  printf("||          Obter Elemento        [5]||\n");
-  printf("||          Obter Posição         [6]||\n");
-  printf("||         Insere Elemento        [7]||\n");
-  printf("||         Retira Elemento        [8]||\n");
-  printf("\nOpção: ");
-  scanf("%d", &opcao);
-  return opcao;
+void exibir (tlista *L){
+    int i;
+    printf("Lista: \" ");
+    for (i = 0; i < L->n; i++){
+        printf("%i ", L->dados[i]);
+    printf("\"\n");
+    }
 }
 
-void opcao(tlista *lista, int op) {
-  switch (op) {
-    case 1:
-      cria_lista(lista, int tam);
-      break;
-    case 2:
-      verifica_vazia(lista);
-      break;
-    case 3:
-      verifica_cheia(lista);
-      break;
-    case 4:
-      tamanho(lista);
-      break;
-    case 5:
-      obter_elemento(lista, int pos);
-      break;
-    case 6:
-      obter_posicao(lista, int pos);
-      break;
-    case 7:
-      insere(lista, pos, dado);
-      break;
-    case 8:
-      retira(lista, int pos);
-      break;
-    default:
-      printf("Comando Invalido\n\n");
-  }
+
+int main() {
+    tlista lista;
+    int tam, pos;
+    float dado;
+    int play = 1;
+
+    do{
+        
+    }
+
+    printf ("Decida o tamanho da Lista: "); scanf ("%d", &tam);
+    cria_lista (&lista, tam);
+
+    verifica_vazia (&lista);
+    verifica_cheia (&lista);
+    tamanho (&lista);
+
+    printf ("Esolha uma posição para achar o elemento: "); scanf ("%d", &pos);
+    obter_elemento (&lista, pos);
+
+    printf ("Esolha uma elemento para achar a posição: "); scanf ("%f", &dado);
+    obter_posicao (&lista, dado);
+
+    printf ("Esolha uma posição e um elemento para adicionar a lista: "); scanf ("%d e %f", &pos, &dado);
+    insere (&lista, pos, dado);
+
+    printf ("Esolha uma posição para retirar da lista: "); scanf ("%d", &pos);
+    retira (&lista, pos);
+    exibir (&lista);
 }
